272516c9062bab88c5633172dcbaf93985dac129
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-24 19:50:29 +0100

    create header blade

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,43 @@
+<header>
+    <div class="container">
+      <div class="navbar">
+        <figure>
+            <a href="#">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="dc-logo">
+            </a>
+        </figure>
+        <ul>
+            <li>
+                <a href="#">characters</a>
+            </li>
+            <li>
+                <a href="#">comics</a>
+            </li>
+            <li>
+                <a href="#">movies</a>
+            </li>
+            <li>
+                <a href="#">tv</a>
+            </li>
+            <li>
+                <a href="#">games</a>
+            </li>
+            <li>
+                <a href="#">collectibles</a>
+            </li>
+            <li>
+                <a href="#">videos</a>
+            </li>
+            <li>
+                <a href="#">fans</a>
+            </li>
+            <li>
+                <a href="#">news</a>
+            </li>
+            <li>
+                <a href="#">shop</a>
+            </li>                 
+        </ul>   
+      </div>
+    </div>
+  </header>
